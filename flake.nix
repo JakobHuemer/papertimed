c7afa35b28ee280f1a4d152282b5a59016c46bb6
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-04-15 09:10:13 +0200

    chore: add rust flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "papertimed - time-based wallpaper daemon";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    rust-overlay,
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      perSystem = {
+        self',
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (pkgs.rust-bin.stable.latest.default.override {
+              extensions = [
+                "rust-src"
+                "rust-analyzer"
+                "clippy"
+                "rustfmt"
+              ];
+            })
+            cargo
+            alejandra
+          ];
+        };
+      };
+    };
+}
